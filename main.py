d024a17285cae809dde55684dcd6bcb2370db5a6
Milan Gurung <71325159+Thebluedragon7@users.noreply.github.com>
2021-06-24 20:11:18 +0000

    Quality Changes

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import requests
 import json
 from discord.ext import commands
 from keep_alive import keep_alive
+import praw
 
-bot = commands.Bot(command_prefix = "?")
-intents = discord.Intents(messages=True, guilds=True, typing = False, presences = False)
-nuk = discord.Client(intents = intents)
+
+
+intents = discord.Intents(messages=True, guilds=True, typing = False, presences = False, members = True)
+nuk = commands.Bot(command_prefix='.',case_insensitive=True, intents = intents)
 print('Logging in...')
 
 def get_facts():
@@ -19,21 +21,30 @@ def get_facts():
 def get_meaning(inWord):
   resp_01 = requests.get('https://api.dictionaryapi.dev/api/v2/entries/en_US/{}'.format(str(inWord)))
   json_data01 = json.loads(resp_01.text)
-
   mnin = "\n".join([f"{i} {inWord}:\n {meaning['definitions'][0]['definition']}\n" for i, meaning in enumerate(json_data01[0]["meanings"])])
 
   return f"```{mnin}```"
   # for meaning in json_data01[0]["meanings"]:
   #   defn = meaning["definitions"][0]["definition"]
   # return defn
+
+def red_get():
+  reddit = praw.Reddit(client_id="9F7ooKfQmGGasg", client_secret="MOu78eIT5ly42iyHAms3s4UddlIfUg",user_agent="PythonCoder123333")
+  submission = reddit.subreddit("CuteTraps").random()
+  return submission.url
+
+
+
 @nuk.event
 async def on_ready():
   print('Logged in as {0.user}'.format(nuk))
 
-@bot.command()
-async def m(ctx, *, wrd):
-  await ctx.send(f'{wrd}')
-  mng = get_meaning(wrd)
+@nuk.command()
+async def mn(ctx, *, wrd):
+  gword = wrd
+  await ctx.wrd.delete()
+  await ctx.send(gword)
+  mng = get_meaning(gword)
   await ctx.send(mng)
 
 
@@ -44,10 +55,10 @@ async def on_message(message):
 
   if message.content == 'gimmehelp':
     await message.channel.send("""
-    \n```
-    **TypoBot Help**\ngimmegreet : greets you
+    >>> TypoBot Help
+    gimmegreet : greets you
     gimmefact  : random facts
-    gimmehelp  : help```
+    gimmehelp  : help
     """)
   elif message.content.startswith('gimmegreet'):
     await message.channel.send('Hello {0.author}'.format(message))
@@ -61,8 +72,16 @@ async def on_message(message):
     fl_wrr = wrr.replace(' ', '%20')
     mnng = get_meaning(fl_wrr)
     await message.channel.send(mnng)
+  elif message.content.startswith('red'):
+    red_sub = red_get()
+    await message.channel.send(red_sub)
 
-  
+# @nuk.command()
+# async def ct(self, ctx):
+#   reddit = praw.Reddit(client_id="9F7ooKfQmGGasg", client_secret="MOu78eIT5ly42iyHAms3s4UddlIfUg",user_agent="PythonCoder123333")
+#   submission = reddit.subreddit("CuteTraps").random()
+#   await ctx.send(submission.url)
+
 
 keep_alive()
 nuk.run(os.environ['TOKEN'])
